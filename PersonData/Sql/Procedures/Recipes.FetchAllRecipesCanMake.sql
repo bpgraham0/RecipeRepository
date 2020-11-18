286f5ac4dd6d5cf967566321a637cd27980d034a
Andrew <Andrew@DESKTOP-1UPPPHP>
2020-11-17 19:02:54 -0600

    R ecipe ingreadent list bad

--- PersonData/Sql/Procedures/Recipes.FetchAllRecipesCanMake.sql
+++ PersonData/Sql/Procedures/Recipes.FetchAllRecipesCanMake.sql
@@ -1,10 +1,19 @@
 ﻿CREATE OR ALTER PROCEDURE Recipes.FetchAllRecipesCanMake
 As
-select R.[Name]
-from Recipes.ingredient I
-Inner Join Recipes.IngredientList IL ON I.ingredientID=IL.ingredientID
-and I.HaveItem='true'
-Inner Join recipe.IL
+select R.[name],FT.[Name],CT.[name],R.Discription,R.CookTime,R.PrepTime,R.ServingSize,Hr.Stars
+from Recipes.Recipe R
+Inner Join Recipes.FoodType FT on FT.RecipeID=R.RecipeID
+Inner Join Recipes.CourseType CT on CT.RecipeID=R.RecipeID
+Inner Join Recipes.HistoryOfusedRecipes HR on HR.RecipeID=R.RecipeID
+Inner Join Recipes.on HR.RecipeID=R.RecipeID
+
+EXCEPT
+
+select IL.RecipieID
+from Recipes.IngreadentList IL
+Inner Join Recipes.Ingredient I On I.IngredientId = IL.IngredientId
+where I.HaveItem ='False'
+
 
 
 
