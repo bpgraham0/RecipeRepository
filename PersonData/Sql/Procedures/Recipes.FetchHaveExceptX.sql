530afed0bf0b4d88a81a0a752ae04e5243ac3cc0
Andrew <Andrew@DESKTOP-1UPPPHP>
2020-11-17 19:57:51 -0600

    done with procedures

---
+++ PersonData/Sql/Procedures/Recipes.FetchHaveExceptX.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR ALTER PROCEDURE Recipes.FetchHaveExceptX
+int @x
+As
+select R.[name],FT.[Name],CT.[name],R.Discription,R.CookTime,R.PrepTime,R.ServingSize,Hr.Stars,Count(I.HaveItem = 'false') As Need
+from Recipes.Recipe R
+Inner Join Recipes.FoodType FT on FT.RecipeID=R.RecipeID
+Inner Join Recipes.CourseType CT on CT.RecipeID=R.RecipeID
+Inner Join Recipes.HistoryOfusedRecipes HR on HR.RecipeID=R.RecipeID
+inner Join Recipes.IngredientList IL on IL.RecipeID =R.RecipeID
+inner join Recipes.Ingredient I on IL.IngredientID = I.IngredientID
+where Need <=@x
+group by R.RecipeID
+
